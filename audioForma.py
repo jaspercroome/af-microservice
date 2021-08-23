6eb6b407038e218d961f061ec733404a2f438b5c
Jasparr77 <jaspercroome@protonmail.com>
2021-08-22 21:00:23 -0700

    Revert "Update audioForma.py"
    This reverts commit efa44dabb645c2d0cdbcafe668b8ea00b551308d.

--- audioForma.py
+++ audioForma.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 
 import os
 from urllib.request import urlopen
+from urllib import request, parse
 
 from librosa import load, cqt, get_duration
 from librosa.effects import hpss
@@ -60,11 +61,11 @@ def songdata():
 
     song_data = c_df_h_final.to_csv(index=False)
 
-    # post_data = parse.urlencode(song_data).encode
+    post_data = parse.urlencode(song_data).encode
 
-    # firebase_url = 'https://audioforma.firebaseio.com/analyzed_songs/'+song_url_id
+    firebase_url = 'https://audioforma.firebaseio.com/analyzed_songs/'+song_url_id
 
-    # req = request.Request(firebase_url,data=post_data)
+    req = request.Request(firebase_url,data=post_data)
 
     return(song_data, 200)
     
